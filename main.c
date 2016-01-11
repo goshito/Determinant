d2daf343624e5f5dd52244e32d3d5d63be118424
Georgi Toshev <georgi.toshev@gmail.com>
2016-01-11 14:29:45 +0200

    Vuvejdane na reda na determinantata i elementite

--- main.c
+++ main.c
@@ -18,6 +18,21 @@
  * 
  */
 int main(int argc, char** argv) {
+        
+    int i, j, n;
+    int determinanta;
+    
+    printf("\nVuvedi red na determinantata: ");
+    scanf("%d", &n);
+    int a[n][n];
+    
+    
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("\nVuvedi element a[%d][%d]=", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
 
     return (EXIT_SUCCESS);
 }
