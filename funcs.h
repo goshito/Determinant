c8fefc84db73ff2a17787f8bdb16fa3850d6ea41
Georgi Toshev <georgi.toshev@gmail.com>
2016-02-03 19:20:33 +0200

    funcs.c and funcs.h created in order to separate the functions

---
+++ funcs.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   funcs.h
+ * Author: georgi
+ *
+ * Created on February 3, 2016, 7:15 PM
+ */
+
+#ifndef FUNCS_H
+#define FUNCS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FUNCS_H */
+
